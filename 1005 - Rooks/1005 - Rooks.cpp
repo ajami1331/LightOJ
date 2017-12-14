7ddbc128e81bded0fe394f7e4034cceda3b75d6e
Araf Al-Jami <CLown1331@users.noreply.github.com>
2017-12-15 02:23:06 +0600

    1005 Rooks

--- 1005 - Rooks/1005 - Rooks.cpp
+++ 1005 - Rooks/1005 - Rooks.cpp
@@ -1,27 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-typedef unsigned long long int ull;
+//using ll = long long;
+const int sz = 1e5 + 10;
+int t, n, k, cs;
+unsigned long long dp[32][32][905];
 
 int main() {
-    #ifdef LU_SERIOUS
-        freopen( "in.txt", "r", stdin );
-//        freopen( "out.txt", "w+", stdout );
-    #endif // LU_SERIOUS
-    int t, cs;
-    ull n, k, ans;
-    cin >> t;
-    for( cs=1; cs<=t; cs++ ) {
-        cin >> n >> k;
-        if( n*n >= k ) {
-            ans = 1;
-            for( ull kk=1; kk<=k; kk++ ) {
-                ans = ans * n*n/kk;
-                --n;
+    #ifdef CLown1331
+        freopen( "in.txt","r", stdin );
+    #endif /// CLown1331
+    for( int i=1; i<31; i++ ) {
+        for( int j=1; j<31; j++ ) {
+            dp[i][j][0] = 1;
+        }
+    }
+    for( int i=1; i<31; i++ ) {
+        for( int j=1; j<31; j++ ) {
+            for( int k=1; k<31; k++ )  {
+                if( k > i || k > j ) dp[i][j][k] = 0;
+                else if( j == 1 ) dp[i][j][k] = i;
+                else dp[i][j][k] = i * dp[i - 1][j - 1][k - 1] + dp[i][j - 1][k];
             }
-        } else ans = 0;
-        cout << "Case " << cs << ": ";
-        cout << ans << "\n";
+        }
+    }
+    scanf( "%d", &t );
+    for( cs=1; cs<=t; cs++ ) {
+        scanf( "%d %d", &n, &k );
+        printf( "Case %d: %llu\n", cs, dp[n][n][k] );
     }
     return 0;
 }
