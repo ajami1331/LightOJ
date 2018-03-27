4528ff11b689bce4d23f29f97569324355bee3e9
Araf Al-Jami <CLown1331@users.noreply.github.com>
2018-03-28 03:23:26 +0600

    Update 1201 - A Perfect Murder.cpp

--- 1201 - A Perfect Murder/1201 - A Perfect Murder.cpp
+++ 1201 - A Perfect Murder/1201 - A Perfect Murder.cpp
@@ -1,59 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long int ll;
-typedef vector<int> VI;
-typedef vector<VI> VVI;
-const int inf = 1e9;
-int clr[1001];
-VVI g;
 
-bool FindMatch(int i, const VVI &w, VI &mr, VI &mc, VI &seen) {
-    for (int j = 0; j < w[i].size(); j++) {
-        if (w[i][j] && !seen[j]) {
-            seen[j] = true;
-            if (mc[j] < 0 || FindMatch(mc[j], w, mr, mc, seen)) {
-                mr[i] = j;
-                mc[j] = i;
-                return true;
-            }
+int t, cs, n, m;
+int fl[1005];
+int match[1005];
+int g[1005][1005];
+
+bool bpm( int u ) {
+        for( int v=0; v<n; v++ ) {
+                if( g[u][v] && !fl[v] ) {
+                        fl[v] = true;
+                        if( match[v] == -1 || bpm( match[v] ) ) {
+                                match[v] = u;
+                                return true;
+                        }
+                }
         }
-    }
-    return false;
+        return false;
 }
 
-int BipartiteMatching(const VVI &w ) {
-    VI mr = VI(w.size(), -1);
-    VI mc = VI(w[0].size(), -1);
-
-    int ct = 0;
-    for (int i = 0; i < w.size(); i++) {
-        VI seen(w[0].size());
-        if (FindMatch(i, w, mr, mc, seen)) ct++;
-    }
-    return ct;
+int max_match() {
+        memset( match, -1, sizeof match );
+        int ret = 0;
+        for( int i=0; i<n; i++ ) {
+                memset( fl, 0, sizeof fl );
+                if( bpm( i ) ) {
+                        ret++;
+                }
+        }
+        return ret;
 }
+
 
 int main() {
-    #ifdef LU_SERIOUS
-        freopen( "in.txt", "r", stdin );
-//        freopen( "out.txt", "w+", stdout );
-    #endif // LU_SERIOUS
-    int t, cs, x, y, n, m, h;
-    scanf( "%d", &t );
-    for( cs=1; cs<=t; cs++ ) {
-        scanf( "%d %d", &n, &m );
-        g = VVI( n, VI( n, 0 ) );
-        while( m-- ) {
-            scanf( "%d %d", &x, &y );
-            --x, --y;
-            g[x][y] = 1;
-            g[y][x] = 1;
+        #ifdef CLown1331
+                freopen( "in.txt", "r", stdin );
+        #endif /// CLown1331
+
+        scanf( "%d", &t );
+
+        for( cs=1; cs<=t; cs++ ) {
+
+                memset( g, 0, sizeof g );
+
+                scanf( "%d %d", &n, &m );
+
+                while( m-- ) {
+                        int x, y;
+                        scanf( "%d %d", &x, &y );
+                        --x, --y;
+                        g[x][y] = 1;
+                        g[y][x] = 1;
+                }
+
+                printf( "Case %d: %d\n", cs, n - max_match() / 2 );
+
         }
-        h = BipartiteMatching( g );
-//        cerr << h << " " << h / 2 << " ";
-        h = n - h / 2;
-        printf( "Case %d: %d\n", cs, h );
-    }
-    return 0;
+        return 0;
 }
