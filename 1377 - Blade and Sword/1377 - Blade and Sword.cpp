38336af5efb4f418d6278124cdfcd69569a236d0
Araf Al-Jami <CLown1331@users.noreply.github.com>
2018-04-02 03:08:26 +0600

    updated

--- 1377 - Blade and Sword/1377 - Blade and Sword.cpp
+++ 1377 - Blade and Sword/1377 - Blade and Sword.cpp
@@ -5,94 +5,90 @@ vector < pair < int, int > > tele;
 int dirx[] = { +0, +0, +1, -1 };
 int diry[] = { +1, -1, +0, +0 };
 char str[201][201];
-int lvl[201][201];
-int vis[201][201];
+int lvl[201][201][2];
+int vis[201][201][2];
 int t, cs, n, m;
 int sx, sy, ex, ey;
 
-struct nd{
-    int x, y, f;
+struct nd {
+        int x, y, f;
 };
 
 bool bfs() {
-    queue < nd > Q;
-    nd hand;
-    int nx, ny;
-    Q.push( { sx, sy, 0 } );
-    vis[sx][sy] = cs;
-    lvl[sx][sy] = 0;
-    while( !Q.empty() ) {
-        hand = Q.front();
-        Q.pop();
-        if( str[hand.x][hand.y] == '*' ) {
-            for( int i=0; i<tele.size(); i++ ) {
-                nx = tele[i].first;
-                ny = tele[i].second;
-                if( ( nx == hand.x && ny == hand.y ) || vis[nx][ny] == cs ) continue;
-                else {
-                    lvl[nx][ny] = lvl[hand.x][hand.y] + 1;
-                    vis[nx][ny] = cs ;
-                    Q.push( { nx, ny, 1 } );
+        queue < nd > Q;
+        nd hand;
+        int nx, ny;
+        Q.push( { sx, sy, 0 } );
+        vis[sx][sy][0] = cs;
+        lvl[sx][sy][0] = 0;
+        while( !Q.empty() ) {
+                hand = Q.front();
+                Q.pop();
+                if( hand.x == ex && hand.y == ey ) return true;
+                if( str[hand.x][hand.y] == '*' ) {
+                        for( int i=0; i<tele.size(); i++ ) {
+                                nx = tele[i].first;
+                                ny = tele[i].second;
+                                if( ( nx == hand.x && ny == hand.y ) || vis[nx][ny][1] == cs )
+                                        continue;
+                                else {
+                                        lvl[nx][ny][1] = lvl[hand.x][hand.y][hand.f] + 1;
+                                        vis[nx][ny][1] = cs ;
+                                        Q.push( { nx, ny, 1 } );
+                                }
+                        }
+                        tele.clear();                                           ///  shb gulah teleport visited,
+                        tele.push_back( { hand.x, hand.y } );                   ///  back asteh parley hoiloh, ar kuno gulah dorkar nai.
                 }
-            }
-            if( hand.f ) {
-                for( int i=0; i<4; i++ ) {
-                    nx = hand.x + dirx[i];
-                    ny = hand.y + diry[i];
-                    if( nx < 0 || nx >= n || ny < 0 || ny >= m || vis[nx][ny] == cs || str[nx][ny] == '#' ) continue;
-                    lvl[nx][ny] = lvl[hand.x][hand.y] + 1;
-                    vis[nx][ny] = cs ;
-                    Q.push( { nx, ny, 0 } );
+                if( str[hand.x][hand.y] != '*' || ( str[hand.x][hand.y] == '*' && hand.f ) ) {
+                        for( int i=0; i<4; i++ ) {
+                                nx = hand.x + dirx[i];
+                                ny = hand.y + diry[i];
+                                if( nx < 0 || nx >= n || ny < 0 || ny >= m || vis[nx][ny][0] == cs || str[nx][ny] == '#' )
+                                        continue;
+                                lvl[nx][ny][0] = lvl[hand.x][hand.y][hand.f] + 1;
+                                vis[nx][ny][0] = cs ;
+                                Q.push( { nx, ny, 0 } );
+                        }
                 }
-            }
-        } else {
-            for( int i=0; i<4; i++ ) {
-                nx = hand.x + dirx[i];
-                ny = hand.y + diry[i];
-                if( nx < 0 || nx >= n || ny < 0 || ny >= m || vis[nx][ny] == cs || str[nx][ny] == '#' ) continue;
-                lvl[nx][ny] = lvl[hand.x][hand.y] + 1;
-                vis[nx][ny] = cs ;
-                Q.push( { nx, ny, 0 } );
-            }
         }
-    }
-    return vis[ex][ey] == cs;
+        return vis[ex][ey][0] == cs;
 }
 
 int main() {
-    #ifdef LU_SERIOUS
+#ifdef CLown1331
         freopen( "in.txt", "r", stdin );
-    #endif // LU_SERIOUS
-    scanf( "%d", &t );
-    for( cs=1; cs<=t; cs++ ) {
-        scanf( "%d %d", &n, &m );
-        tele.clear();
-        for( int i=0; i<n; i++ ) {
-            scanf( "%s", str[i] );
-        }
-        for( int i=0; i<n; i++ ) {
-            for( int j=0; j<m; j++ ) {
-//                cerr << str[i][j] << " \n"[ j==m-1 ];
-                if( str[i][j] == '*' ) {
-                    tele.push_back( { i, j } );
-                }
-                if( str[i][j] == 'P' ) {
-                    sx = i;
-                    sy = j;
-                }
-                if( str[i][j] == 'D' ) {
-                    ex = i;
-                    ey = j;
-                }
-            }
-        }
-        if( bfs() ) printf( "Case %d: %d\n", cs, lvl[ex][ey] );
-        else {
-            if( bfs2() ) {
-                printf( "Case %d: %d\n", cs, lvl[ex][ey] );
-            } else printf( "Case %d: impossible\n", cs );
-        }
-    }
+#endif /// CLown1331
 
-    return 0;
+        scanf( "%d", &t );
+
+        for( cs=1; cs<=t; cs++ ) {
+                scanf( "%d %d", &n, &m );
+                tele.clear();
+                for( int i=0; i<n; i++ ) {
+                        scanf( "%s", str[i] );
+                }
+                for( int i=0; i<n; i++ ) {
+                        for( int j=0; j<m; j++ ) {
+                                if( str[i][j] == '*' ) {
+                                        tele.push_back( { i, j } );
+                                }
+                                if( str[i][j] == 'P' ) {
+                                        sx = i;
+                                        sy = j;
+                                }
+                                if( str[i][j] == 'D' ) {
+                                        ex = i;
+                                        ey = j;
+                                }
+                        }
+                }
+                if( bfs() )
+                        printf( "Case %d: %d\n", cs, lvl[ex][ey][0] );
+                else
+                        printf( "Case %d: impossible\n", cs );
+
+        }
+
+        return 0;
 }
